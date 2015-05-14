57efd27c1802c22e532d803504d4cf0cc0372b4c
Tyler Wymer <twymer@gmail.com>
2015-05-14 15:11:58 -0500

    Restaurants have many meals

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,4 @@
 class Restaurant < ActiveRecord::Base
+  has_many :locations
+  has_many :meals, through: :locations
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has many meals" do
+    steak = Meal.create(name: "Unicorn steak")
+    shake = Meal.create(name: "Koolaid shake")
+    fries = Meal.create(name: "Metal shaving fries")
+
+    menu = Menu.create(name: "Totally not made up menu")
+
+    MenuItem.create(menu: menu, meal: steak)
+    MenuItem.create(menu: menu, meal: shake)
+    MenuItem.create(menu: menu, meal: fries)
+
+    restaurant = Restaurant.create(name: "Unicorn Steak & Shake")
+    Location.create(
+      address: "Winterfell",
+      menu: menu,
+      restaurant: restaurant
+    )
+
+    expect(restaurant.meals).to match_array([steak, shake, fries])
+  end
 end
