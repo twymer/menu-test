a2b803260386122326bd9eebea8c310bc727bc26
Tyler Wymer <twymer@gmail.com>
2015-05-14 15:05:03 -0500

    Menus have many meals

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Menu, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has many meals" do
+    steak = Meal.create(name: "Unicorn steak")
+    shake = Meal.create(name: "Koolaid shake")
+    fries = Meal.create(name: "Metal shaving fries")
+
+    menu = Menu.create(name: "Totally not made up menu")
+
+    MenuItem.create(menu: menu, meal: steak)
+    MenuItem.create(menu: menu, meal: shake)
+    MenuItem.create(menu: menu, meal: fries)
+
+    expect(menu.meals).to match_array([steak, shake, fries])
+  end
 end
